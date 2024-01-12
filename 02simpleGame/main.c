dbb397348ce6a94018025096b5f03b7a22aa9700
MarkelGomezz <markel.gomezr@alumni.mondragon.edu>
2024-01-12 08:57:10 +0100

    Titulua buketuta

---
+++ 02simpleGame/main.c
@@ -0,0 +1,15 @@
+
+#include "./lib/game02.h"
+#include "ourTypes.h"
+#include "text.h"
+#include <SDL.h>
+#include <stdio.h>
+
+int pantailaHasi();
+
+int SDL_main(int argc, char *str[])
+{
+    pantailaHasi();
+
+    return 0;
+}
